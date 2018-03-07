2e4ed053f1577f052509d41049dcdd939fef62cb
gameyuki <yuki.game091425@gmail.com>
2018-03-08 00:37:39 +0900

    ObjectBase実装しました。

---
+++ Library/Library/Library/ObjectBase/ObjectBase.cpp
@@ -0,0 +1,33 @@
+#include"ObjectBase.h"
+
+//---------------------------------------------
+//	コンストラクタ	デストラクタ
+//---------------------------------------------
+ObjectBase::ObjectBase()
+{
+}
+
+
+//---------------------------------------------
+//	Public Functions
+//---------------------------------------------
+ObjectBase::~ObjectBase()
+{
+}
+
+bool ObjectBase::Initialize()
+{
+	return true;
+}
+
+void ObjectBase::Finalize()
+{
+}
+
+void ObjectBase::Update()
+{
+}
+
+void ObjectBase::Draw()
+{
+}
--- Library/Library/Library/ObjectBase/ObjectBase.h
+++ Library/Library/Library/ObjectBase/ObjectBase.h
@@ -1,1 +1,43 @@
-#pragma once
+#ifndef OBJECTBASE_H
+#define OBJECTBASE_H
+
+
+/*
+*	オブジェクトの基底クラス
+*/
+class ObjectBase
+{
+public:
+	/*
+	*	コンストラクタ
+	*/
+	ObjectBase();
+
+	/*
+	*	デストラクタ
+	*/
+	virtual ~ObjectBase();
+
+	/*
+	*	初期化関数
+	*	成功すればtrue 失敗すればfalse
+	*/
+	virtual bool Initialize();
+
+	/* 
+	*	終了関数
+	*/
+	virtual void Finalize();
+
+	/*
+	*	更新関数
+	*/
+	virtual void Update();
+
+	/*
+	*	描画関数
+	*/
+	virtual void Draw();
+};
+
+#endif
