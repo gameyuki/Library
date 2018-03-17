c44c2cebda33da549b0311371fdbcf03ef9bdb49
gameyuki <yuki.game091425@gmail.com>
2018-03-18 02:40:03 +0900

    DirectX初期化用クラス実装しました

---
+++ Library/Library/Library/DirectX9/DirectGraphicsDevice/DirectGraphicsDevice.cpp
@@ -0,0 +1,107 @@
+//--------------------------------------------------
+//	include
+//--------------------------------------------------
+#include"DirectGraphicsDevice.h"
+#include"Library\MyDefine\macro.h"
+
+//--------------------------------------------------
+//	コンストラクタ : デストラクタ
+//--------------------------------------------------
+DirectGraphicsDevice::DirectGraphicsDevice(HWND _hWnd,float w, float h, bool f):m_hWnd(_hWnd),m_Width(w),m_Height(h),m_Fullscreen(f)
+{
+}
+
+DirectGraphicsDevice::~DirectGraphicsDevice()
+{
+}
+
+//--------------------------------------------------
+//	public function
+//--------------------------------------------------
+bool DirectGraphicsDevice::Initialize()
+{
+	if (FAILED(m_pDirect3D = Direct3DCreate9(D3D_SDK_VERSION)))
+	{
+		OutPutError("Direct3Dオブジェクトの生成に失敗しました");
+		return false;
+	}
+
+	if (FAILED(m_pDirect3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &m_D3DdisplayMode)))
+	{
+		OutPutError("DisplayModeの取得に失敗しました");
+		return false;
+	}
+
+	InitD3Dpp();
+
+	/* HALMode */
+	if (FAILED(m_pDirect3D->CreateDevice(
+		D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_HAL,
+		m_hWnd,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+		&m_D3dpp, &m_pD3Device)))
+	{
+		/* REFmode */
+		if (FAILED(m_pDirect3D->CreateDevice(
+			D3DADAPTER_DEFAULT,
+			D3DDEVTYPE_REF,
+			m_hWnd,
+			D3DCREATE_HARDWARE_VERTEXPROCESSING,
+			&m_D3dpp, &m_pD3Device)))
+		{
+			return false;
+		}
+
+		SetRenderlingConfiguration();
+
+		return true;
+	}
+}
+
+void DirectGraphicsDevice::Finalize()
+{
+	SafeRelease(m_pD3Device);
+	SafeRelease(m_pDirect3D);
+}
+
+void DirectGraphicsDevice::BeginScene()
+{
+	//　相談
+	m_pD3Device->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+	m_pD3Device->BeginScene();
+}
+
+void DirectGraphicsDevice::EndScene()
+{
+	m_pD3Device->EndScene();
+	m_pD3Device->Present(NULL, NULL, NULL, NULL);
+}
+
+//--------------------------------------------------
+//	private function
+//--------------------------------------------------
+void DirectGraphicsDevice::InitD3Dpp()
+{
+	ZeroMemory(&m_D3dppwnd, sizeof(D3DPRESENT_PARAMETERS));
+	m_D3dppwnd.BackBufferWidth = m_Width;
+	m_D3dppwnd.BackBufferHeight = m_Height;
+	m_D3dppwnd.BackBufferFormat = m_D3DdisplayMode.Format;
+	m_D3dppwnd.BackBufferCount = 1;
+	m_D3dppwnd.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	m_D3dppwnd.Windowed = TRUE;
+	//岡田さんのコードを見る
+
+	m_D3dpp = m_Fullscreen ? m_D3dppfull : m_D3dppwnd;
+}
+
+void DirectGraphicsDevice::SetRenderlingConfiguration()
+{
+	m_pD3Device->SetRenderState(D3DRS_ALPHABLENDENABLE, true);
+	m_pD3Device->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	m_pD3Device->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
+}
---
+++ Library/Library/Library/DirectX9/DirectGraphicsDevice/DirectGraphicsDevice.h
@@ -0,0 +1,137 @@
+#ifndef DIRECTGRAPHICSDEVICE_H
+#define	DIRECTGRAPHICSDEVICE_H
+
+//--------------------------------------------------
+//	include
+//--------------------------------------------------
+#include<Windows.h>
+#include<d3d9.h>
+#include<d3dx9.h>
+
+#include"Library\SingletonBase\SingletonBase.h"
+
+//--------------------------------------------------
+//	class
+//--------------------------------------------------
+/* 
+*	Direct3D初期化用クラス
+*/
+class DirectGraphicsDevice : public SingletonBase<DirectGraphicsDevice>
+{
+public:
+	//--------------------------------------------------
+	//	public function
+	//--------------------------------------------------
+	/*
+	*	デストラクタ
+	*/
+	~DirectGraphicsDevice();
+
+	/*
+	*	初期化用関数
+	*	成功すればtrue 失敗すればfalse
+	*/
+	bool Initialize();
+
+	/*
+	*	解放関数
+	*/
+	void Finalize();
+
+	/*
+	*	デバイスアクセス
+	*/
+	inline LPDIRECT3DDEVICE9 GetD3Device9()
+	{
+		return m_pD3Device;
+	}
+
+	/*
+	*	描画開始用
+	*	各シーンに
+	*/
+	void BeginScene();
+
+	/*
+	*	ページフリッピング用
+	*	//相談	名前
+	*/
+	void EndScene();
+
+private:
+	//--------------------------------------------------
+	//	private function
+	//--------------------------------------------------
+	/*
+	*	隠しコンストラクタ
+	*	第一引数	ウィンドウハンドル
+	*	第二引数	ウインドウの横の長さ
+	*	第三引数	ウインドウの縦の長さ
+	*	第四引数	ウインドウがフルスクリーンか否か
+	*	true ならフルスクリーン falseならウインドウサイズ
+	*/
+	DirectGraphicsDevice(HWND _hWnd,float w,float h,bool f);
+
+	/*
+	*	パラメーター初期化
+	*/
+	void InitD3Dpp();
+
+	/*
+	*	描画用設定
+	*/
+	void SetRenderlingConfiguration();
+
+	//--------------------------------------------------
+	//	private variable
+	//--------------------------------------------------
+	/*
+	*	Direct3D用インターフェース
+	*/
+	LPDIRECT3D9 m_pDirect3D;
+
+	/*
+	*	Direct3DDevice用インターフェース
+	*/
+	LPDIRECT3DDEVICE9 m_pD3Device;
+
+	/*
+	*	Direct3Dパラメーター用インターフェース
+	*/
+	D3DPRESENT_PARAMETERS m_D3dpp;
+
+	/*
+	*	ウィンドウサイズ用
+	*/
+	D3DPRESENT_PARAMETERS m_D3dppwnd;
+
+	/*
+	*	フルスクリーン用
+	*/
+	D3DPRESENT_PARAMETERS m_D3dppfull;
+
+	/*
+	*	Direct3Dパラメーター用ディスプレイインターフェース
+	*/
+	D3DDISPLAYMODE	m_D3DdisplayMode;
+
+	/*
+	*	ハンドル
+	*/
+	HWND m_hWnd;
+
+	/*
+	*	ウィンドウの縦と横
+	*/
+	float m_Width;
+	float m_Height;
+
+	/*
+	*	フルスクリーンか否か
+	*	フルスクリーンならtrue
+	*	ウィンドウサイズならfalse
+	*/
+	bool m_Fullscreen;
+};
+
+#endif
